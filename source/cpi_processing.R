0fd786567a126be373867fa222be902a2b527387
Rara Soro <raras@spc.int>
2025-04-07 11:30:50 +1100

    More changes to the cpi-processing script adding the monthly seasonal changes

--- source/cpi_processing.R
+++ source/cpi_processing.R
@@ -104,6 +104,61 @@ mmSeasonal <- mmSeasonal |>
   ) |>
   select(-Division, -Month, -period, -SAdjusted)
 
+#### ************************** Table monthly seasonal changes ********************************** ####
+
+mmChange <- read_excel("../data/cpi_data.xlsx", sheet = "mmChange")
+
+colHeader <- colnames(mmChange)[2]
+selection <- mmChange |>
+  select(ITEM, colHeader)
+  #rename(ITEM = item)
+
+selection$TIME_PERIOD <- colHeader
+colnames(selection)[2] <- "OBS_VALUE"
+
+#Get first record
+mmIndex <- selection |>
+  mutate(FREQ = ifelse(nchar(TIME_PERIOD) == 4, "A", "M"),
+         REF_AREA = "FJ",
+         INDICATOR = "IDX",
+         TRANSFORMATION = ifelse(nchar(TIME_PERIOD)==4, "GIY", "G1M"),
+         SEASONAL_ADJUST = "S",
+         UNIT_MEASURE = "INDEX",
+         BASE_YEAR = "",
+         OBS_STATUS = "",
+         COMMENT = "",
+         DECIMALS = 1
+  )
+
+index = 3
+total_columns <- ncol(mmChange)
+
+#Loop to get the other columns
+
+while (index <= total_columns){
+  ncolHead <- colnames(mmChange)[index]
+  nextData <- mmChange |> select(ITEM, ncolHead)
+  nextData$TIME_PERIOD <- ncolHead
+  colnames(nextData)[2] <- "OBS_VALUE"
+  nextData$OBS_VALUE <- as.numeric(nextData$OBS_VALUE)
+  nextData <- nextData |>
+    mutate(FREQ = ifelse(nchar(TIME_PERIOD) == 4, "A", "M"),
+           REF_AREA = "FJ",
+           INDICATOR = "IDX",
+           TRANSFORMATION = ifelse(nchar(TIME_PERIOD)==4, "GIY", "G1M"),
+           SEASONAL_ADJUST = "S",
+           UNIT_MEASURE = "INDEX",
+           BASE_YEAR = "",
+           OBS_STATUS = "",
+           COMMENT = "",
+           DECIMALS = 1
+    )
+  
+  mmIndex <- rbind(mmIndex, nextData)
+  index <- index + 1
+}
+
+
 
 
 
