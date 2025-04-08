5f61c3f7be12c7bd2b1b4d1fe102d95f76f3446d
Rara Soro <raras@spc.int>
2025-04-09 09:05:30 +1100

    Added the code to read in and process the real gdp percentage change

--- source/gdp_processing.R
+++ source/gdp_processing.R
@@ -9,7 +9,7 @@ setwd(repository)
 
 #### ************************** Table base weight processing ********************************** ####
 
-rgdpWeight <- read_excel("../data/gdp_data.xlsx", sheet = "RGDP_VAL")
+rgdpWeight <- read_excel("../data/rgdp_data.xlsx", sheet = "RGDP_VAL")
 rgdpWeight_DT <- rgdpWeight |>
   select(id, bWeight) |>
   rename(INDUSTRY_TYPE = id, OBS_VALUE = bWeight) |>
@@ -46,7 +46,7 @@ rgdp <- selection |>
   mutate(FREQ = "A",
          REF_AREA = "FJ",
          INDICATOR = "RLGDP",
-         TRANSFORMATION = "YM1",
+         TRANSFORMATION = "",
          UNIT_MEASURE = "FJD",
          UNIT_MULT = 6,
          OBS_STATUS =  ifelse(grepl("r", TIME_PERIOD), "R",
@@ -71,7 +71,7 @@ while (index <= total_columns){
     mutate(FREQ = "A",
            REF_AREA = "FJ",
            INDICATOR = "RLGDP",
-           TRANSFORMATION = "YM1",
+           TRANSFORMATION = "",
            UNIT_MEASURE = "FJD",
            UNIT_MULT = 6,
            OBS_STATUS = ifelse(grepl("r", TIME_PERIOD), "R",
@@ -93,6 +93,72 @@ rgdp_DT <- rgdp |>
 #Combine the datatables together
 
 realgdp <- rbind(rgdpWeight_DT, rgdp_DT)
+
+#### ******************************** Real gdp percentage processing ***************************************** ####
+
+gdpPercent <- read_excel("../data/rgdp_data.xlsx", sheet = "RGDP_PER")
+
+colHeader <- colnames(gdpPercent)[3]
+selection <- gdpPercent |>
+  select(id, colHeader) |>
+  rename(INDUSTRY_TYPE = id)
+
+selection$TIME_PERIOD <- colHeader
+colnames(selection)[2] <- "OBS_VALUE"
+
+#Get first record
+rgdpPercent <- selection |>
+  mutate(FREQ = "A",
+         REF_AREA = "FJ",
+         INDICATOR = "RLGDP",
+         TRANSFORMATION = "YM1",
+         UNIT_MEASURE = "PERCENT",
+         UNIT_MULT = "",
+         OBS_STATUS =  ifelse(grepl("r", TIME_PERIOD), "R",
+                              ifelse(grepl("p", TIME_PERIOD), "P", "")),
+         DATA_SOURCE = "",
+         OBS_COMMENT = "",
+         DECIMALS = 1,
+         TIME_PERIOD = substr(TIME_PERIOD, 1, 4)
+  )
+
+index = 4
+total_columns <- ncol(gdpPercent)
+
+#Loop to get the other columns
+
+while (index <= total_columns){
+  ncolHead <- colnames(gdpPercent)[index]
+  nextData <- gdpPercent |> select(id, ncolHead)
+  nextData$TIME_PERIOD <- ncolHead
+  colnames(nextData)[2] <- "OBS_VALUE"
+  nextData$OBS_VALUE <- as.numeric(nextData$OBS_VALUE)
+  nextData <- nextData |>
+    mutate(FREQ = "A",
+           REF_AREA = "FJ",
+           INDICATOR = "RLGDP",
+           TRANSFORMATION = "YM1",
+           UNIT_MEASURE = "PERCENT",
+           UNIT_MULT = "",
+           OBS_STATUS = ifelse(grepl("r", TIME_PERIOD), "R",
+                               ifelse(grepl("p", TIME_PERIOD), "P", "")),
+           DATA_SOURCE = "",
+           OBS_COMMENT = "",
+           DECIMALS = 1,
+           TIME_PERIOD = substr(TIME_PERIOD, 1, 4)
+    ) |>
+    rename(INDUSTRY_TYPE = id)
+  
+  rgdpPercent <- rbind(rgdpPercent, nextData)
+  index <- index + 1
+}
+
+#Append both the real gdp and percentage change together
+
+realGDP_combine <- rbind(realgdp, rgdpPercent)
+
+realGDP_combine <- realGDP_combine |>
+  select(FREQ, TIME_PERIOD, REF_AREA, INDICATOR, TRANSFORMATION, INDUSTRY_TYPE, OBS_VALUE, UNIT_MEASURE, UNIT_MULT, OBS_STATUS, DATA_SOURCE, OBS_COMMENT)
 
 
 
