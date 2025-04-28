6c5af413f86b4ee75130b80fe32e2d8515ae7dfd
Rara Soro <raras@spc.int>
2025-04-29 08:57:11 +1100

    Make changes to the sheetseries script

--- source/sheetSeries.R
+++ source/sheetSeries.R
@@ -23,33 +23,37 @@ i <- 1
 while (i <= length(sheets)) {
   # Read the current sheet
   sheet <- read_excel(file_path, sheet = sheets[i])
-  sheet <- merge(data, bopAcc, by = "label")  
+  sheet <- merge(sheet, bopAcc, by = "label")
+  #Move id column to the first column
+  sheet <- sheet |> select(last_col(), everything())
   
-  colHeader <- colnames(sheet)[3]
+  colHeader <- colnames(sheet)[4]
   selection <- sheet |>
     select(Id, colHeader) |>
     rename(ACCOUNT = Id)
   
   selection$TIME_PERIOD <- colHeader
   colnames(selection)[2] <- "OBS_VALUE"
+  selection$FREQ = ifelse(nchar(selection$TIME_PERIOD)==4, "A", "Q")
+  selection$OBS_STATUS <- ifelse(grepl("r", selection$TIME_PERIOD), "R",
+                                ifelse(grepl("p", selection$TIME_PERIOD), "P", ""))
+  selection$TIME_PERIOD <- gsub("\\s*\\[(r|p)\\]", "", selection$TIME_PERIOD)
   
   #Process first record
   
   datatable <- selection |>
     #arrange(order) |>
-    mutate(FREQ = ifelse(nchar(TIME_PERIOD)==4, "A", "Q"),
-           REF_AREA = "FJ",
+    mutate(REF_AREA = "FJ",
            INDICATOR = "AMT",
            UNIT_MEASURE = "FJD",
            UNIT_MULT = 6,
-           OBS_STATUS = "",
            OBS_COMMENT = "",   
            DECIMALS = 1
            
     )
   
 # Process the rest of the columns in the selected sheet  
-  index = 4
+  index = 5
   total_columns <- ncol(sheet)
   
   while (index <= total_columns){
@@ -57,18 +61,18 @@ while (i <= length(sheets)) {
     nextData <- sheet |> select(Id, ncolHead) |> rename(ACCOUNT = Id)
     nextData$TIME_PERIOD <- ncolHead
     colnames(nextData)[2] <- "OBS_VALUE"
+    nextData$FREQ <- ifelse(nchar(nextData$TIME_PERIOD) ==4, "A", "Q")
+    nextData$OBS_STATUS <- ifelse(grepl("r", nextData$TIME_PERIOD), "R",
+                                  ifelse(grepl("p", nextData$TIME_PERIOD), "P", ""))
+    nextData$TIME_PERIOD <- gsub("\\s*\\[(r|p)\\]", "", nextData$TIME_PERIOD)
     nextData <- nextData |>
       #arrange(order) |>
-      mutate(FREQ = ifelse(nchar(TIME_PERIOD)==4, "A", "Q"),
-             REF_AREA = "FJ",
+      mutate(REF_AREA = "FJ",
              INDICATOR = "AMT",
              UNIT_MEASURE = "FJD",
              UNIT_MULT = 6,
-             OBS_STATUS = ifelse(grepl("r", TIME_PERIOD), "R",
-                                 ifelse(grepl("p", TIME_PERIOD), "P", "")),
              OBS_COMMENT = "",
              DECIMALS = 1,
-             TIME_PERIOD = gsub("\\s*\\[(r|p)\\]", "", TIME_PERIOD)
       )
       
     datatable <- rbind(datatable, nextData)
@@ -79,12 +83,10 @@ while (i <= length(sheets)) {
     select(FREQ, REF_AREA, INDICATOR, ACCOUNT, TIME_PERIOD, OBS_VALUE, UNIT_MEASURE, UNIT_MULT, OBS_STATUS, OBS_COMMENT, DECIMALS)
   
   #Write final final to the csv ouput file
-  write.csv(datatable, "../output/bop/sheets/",sheets[i],".csv")
-
-  # Process the data (example: just print first few rows)
-  #cat("Processing sheet:", sheets[i], "\n")
-  #print(head(data))
+  output_file <- paste0("../output/bop/sheets/" ,sheets[i], ".csv")
   
+  write.csv(datatable, output_file, row.names = FALSE)
+
   # Move to next sheet
   i <- i + 1
 }
